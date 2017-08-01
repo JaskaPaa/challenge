2d3752a3ce766e78b020e91375569d45ef4db4c4
Jaska Pääkkönen <jaska.paakkonen@gmail.com>
2017-08-01 12:04:39 +0300

    Version 1.0

--- src/SJW/SearchBundle/Controller/DefaultController.php
+++ src/SJW/SearchBundle/Controller/DefaultController.php
@@ -8,28 +8,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
+function build_sorter($key) {
+    
+    return function ($a, $b) use ($key) {
+        return strnatcmp( strtolower($a[$key]), strtolower($b[$key]) );
+    };
+}
+
+
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     *
-     * @Template()
-     */
-    public function indexAction()
-    {
-        return array();
-    }
 
-    /**
-     * @Route("/api/search")
-     *
-     * @Template()
-     */
-    public function searchAction(Request $request) {
-        // Get the search string from the UI.
-        $searchString = $request->query->get('q');
-
+    private function getPostalArray() {
+        
         // Read resource file.
+
         $kernel = $this->get('kernel');
 
         $filePath = $kernel->locateResource('@SJWSearchBundle/Resources/data/numbers.txt');
@@ -42,10 +35,94 @@ class DefaultController extends Controller
         foreach($lines as $line) {
             $numbers[] = explode(';', $line);
         }
+        // sort by population
+        usort($numbers, function ($a, $b) {
+            return strnatcmp( strtolower($a[2]), strtolower($b[2]) );
+        });
 
-        // TODO: Implement search based on query string.
+        return $numbers;
+    }
+
+    /**
+     * @Route("/")
+     *
+     * @Template()
+     */
+
+    public function indexAction()
+    {
+        return array();
+    }
+
+    /**
+     * @Route("/api/search")
+     *
+     * @Template()
+     */
+    public function searchAction(Request $request) {
+
+        
+        $searchString = trim($request->query->get('q'));
+        $input = explode('|', $request->query->get('q'));
+        $searchString = trim($input[0]);
+        
+        if (strlen($searchString) == 0 || !ctype_digit($input[1])) {
+            return new JsonResponse(array());
+        }
+
+        $n = intval($input[1]);
+
+        
+        $numbers = $this->getPostalArray();
+
+        $field =  ctype_digit($searchString) ? 0 : 1;
+
+        $key = array_search($searchString, array_column($numbers, $field));
+
+        if ($key === false)
+           return new JsonResponse(array()); 
+
+        $respo = array_slice($numbers, $key - $n, 2*$n + 1);
 
         // Output content.
-        return new JsonResponse($numbers);
+        return new JsonResponse($respo);
     }
+
+    /**
+     * @Route("/api/complete")
+     */
+
+    public function autoComplete(Request $request) {
+
+        $searchString = trim($request->query->get('term'));
+
+        $numbers = $this->getPostalArray();
+
+        
+        // if search is a number use $field 0 else 1
+        $field =  ctype_digit($searchString) ? 0 : 1;
+
+        // filter search mathces from first char  
+        $res = array_filter($numbers, function($k) use ($numbers, $searchString, $field) {
+            $pos = strpos(strtolower($numbers[$k][$field]), strtolower($searchString));
+            return $pos !== false and $pos == 0;
+        }, ARRAY_FILTER_USE_KEY);
+
+        //error_log( array_keys($res)[0] . " ---\n", 3, "/home/jaska/koodi/phplog");
+
+        $keys = array_keys($res);
+
+        $respo = array();
+
+        foreach($keys as $key) {
+            $respo[] = $numbers[$key][$field];
+        }
+
+        sort($respo);
+
+        // Output the first 25
+        return new JsonResponse(array_slice($respo, 0, 25));
+
+    }
+    
 }
--- src/SJW/SearchBundle/Resources/public/js/search.js
+++ src/SJW/SearchBundle/Resources/public/js/search.js
@@ -1,18 +1,59 @@
-;(function($) {
+(function($) {
+    
+    function showTable(data, val) {
+
+        $("#resu tr").empty();
+        $('#resu').append('<thead><tr><th>Zip</th><th>Town</th><th>Population</th></tr></thead>');
+
+        for (i = 0; i < data.length; i++) {
+            $('#resu tr:last').after('<tr><td>' 
+                + data[i][0] + '</td><td>'+ data[i][1] 
+                + '</td><td>'+ data[i][2] + '</td></tr>');
+            if (val == data[i][0] || val == data[i][1])
+               $("#resu tr:nth-child(" + (i + 2) + ")").css("background", "#ddd");
+        }
+    }
+
+    function doSearch(auto_val, spin_val) {
+        // input values from autocomplete and spinner
+        $.ajax({
+            url: 'api/search?q=' + auto_val + "|" + spin_val,
+            type: 'GET',
+            success: function(data) {
+                showTable(data, auto_val);
+            }
+        });
+    }
+
     $(function() {
-        $('.js-do-search').click(function(event) {
-            event.preventDefault();
-
-            var search = $('#search-input').val();
-
-            $.ajax({
-                url: 'api/search?q=' + search,
-                type: 'GET',
-                success: function(data) {
-                    // TODO: implement showing of data.
-                    console.log(data);
-                }
-            });
+        $( "#spinner" ).spinner({
+            min: 0,
+            max: 100,
+            spin: function( event, ui ) {
+                doSearch($("#areas.ui-autocomplete-input").val(), ui.value);
+            }
         });
     });
+
+    $(function() {
+        $("#areas").autocomplete({
+            source: "api/complete",
+            minLength: 1,
+            autoFocus: true,
+            delay: 50,
+            select: function( event, ui ) {
+                doSearch(ui.item.value, $("#spinner").spinner("value"));
+            }
+        });
+          
+    });
+
+    $(function() {
+        $("#areas").keypress(function(e) {
+            if(e.which == 13) { // enter
+                doSearch($("#areas.ui-autocomplete-input").val(), $("#spinner").spinner("value"));
+            }
+        });
+    });
+    
 })(window.jQuery);
